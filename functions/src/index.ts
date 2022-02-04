4fe7308259fb7cde17accd7e3506e6de168a8100
Gustavo Condezo Camayo <gustavocondezocamayo@gmail.com>
2022-02-03 19:44:45 -0500

    Add mode online default

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from "firebase-functions";
-import {initializeApp} from "firebase-admin";
+import {firestore, initializeApp} from "firebase-admin";
 
 initializeApp();
 
@@ -7,4 +7,21 @@ export const firstFunctionHttp = functions.https.onRequest((req, res) => {
   res.send("ok");
 });
 
+export const onCreateUsers = functions.firestore.document("users/{userId}")
+    .onCreate(async (snapshot, context) => {
+    // return snapshot.ref.update({createdDate: new Date()});
+      await firestore().doc("users/" + snapshot.id)
+          .update({createdDate: new Date});
+    });
+
+export const onUpdateUser = functions.firestore.document("users/{userId")
+    .onUpdate(async (change, context) => {
+      await change.after.ref.update({updatedDate: new Date()});
+    });
+
+export const readUser = async () => {
+  const data = await firestore().doc("users/567").get();
+  data;
+  return {prueba: 123};
+};
 
--- functions/src/test/index.test.ts
+++ functions/src/test/index.test.ts
@@ -1,8 +1,25 @@
+import * as path from "path";
 /* First before anything else import 'firebase-functions-test'
  and before initialize it offline or online.*/
 import * as firebaseFunctionsTest from "firebase-functions-test";
-// We start FirebaseFunctionsTest offline
-firebaseFunctionsTest();
+
+// ------------------MODE OFFLINE---------
+// this mode not write in firestore or rtdb products, not conect with cloud
+// Optionally could be connected whit local firestore emulator
+// process.env.FIRESTORE_EMULATOR_HOST = "localhost:8080";
+// const fireTest = firebaseFunctionsTest()
+
+// ------------------end config mode OFFLINE-------------
+
+// -------------------------------------- MODE ONLINE-------------------------
+// We start FirebaseFunctionsTest online (Mode recommended and easy) this affect Firestore , RTDB, and other products, use whit precaution!!!!!
+const fireTest = firebaseFunctionsTest({ // data from firebase config
+  databaseURL: "YOUR_URL",
+  projectId: "YOUR",
+  storageBucket: "YOUR",
+}, path.join(__dirname, "google-key.json")); // Link to file key get from
+// firebase>config>account service> sdk of fire admin > generate new private kesy, donwload and save safe. NOT SHARE FILE.
+// --------------------- end config mode online--------------
 
 /*
 Then we import the file that initializes the Cloud Functions
@@ -10,8 +27,8 @@ Then we import the file that initializes the Cloud Functions
 of 'firebase-admin' inside it.
 If it is in another location, import it.
 */
-import {firstFunctionHttp} from "../index";
-
+import {firstFunctionHttp, onCreateUsers, onUpdateUser, readUser} from "../index";
+import {firestore} from "firebase-admin";
 
 describe("Mi first Test Cloud Function with Jest and Typescript", () => {
   test("Call the function fistFunctionHttp must return \"ok\"", () => {
@@ -24,5 +41,52 @@ describe("Mi first Test Cloud Function with Jest and Typescript", () => {
     } as any;
 
     firstFunctionHttp(req, res);
+  });
+});
+
+describe("Test firestore path/Users", () => {
+  const pathUser = "users/1234";
+
+  // Create the document of test
+  beforeAll(async () => {
+    await firestore().doc(pathUser).set({
+      name: "Hola",
+    });
+  });
+  // delete document of the test
+  afterAll(async () => {
+    await firestore().doc(pathUser).delete({exists: true});
+  });
+  test("call function user return objt with field prueba", async () => {
+    const ret = await readUser();
+    expect(typeof ret == "object").toBe(true);
+    expect(ret).toHaveProperty("prueba");
+  });
+
+  test("When create a new User add field \"createdDate\" whit date actual",
+      async () => {
+        const wrapped = fireTest.wrap(onCreateUsers);
+        const snap = fireTest.firestore.makeDocumentSnapshot({
+          prueba: "no debe estar vacio",
+        }, pathUser);
+        await wrapped(snap);
+        const {createdDate} = (await firestore().doc(pathUser).get()).data() as { createdDate: firestore.Timestamp };
+        const dateNow = new Date();
+        // created recent
+        expect(dateNow.getTime() - createdDate.toDate().getTime()).toBeLessThanOrEqual(10000);
+      });
+
+  test("When update user set updatedDate with time now", async () => {
+    const beforeSnap = fireTest.firestore.makeDocumentSnapshot({name: "Gustavo"}, pathUser);
+    const afterSnap = fireTest.firestore.makeDocumentSnapshot({name: "Gustavo Condezo"}, pathUser);
+    const change = fireTest.makeChange(beforeSnap, afterSnap);
+
+    const wrapped = fireTest.wrap(onUpdateUser);
+
+    await wrapped(change);
+
+    const userDateNow = (await firestore().doc(pathUser).get()).get("updatedDate");
+    const dateNow = new Date();
+    expect(dateNow.getTime() - userDateNow.toDate().getTime()).toBeLessThanOrEqual(10000);
   });
 });
